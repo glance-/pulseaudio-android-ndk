92e77dc253204c1078cb7d7c55b956fadaffa256
Anton Lundin <glance@acc.umu.se>
2017-02-06 23:00:13 +0100

    Abstract out versions to variables
    Signed-off-by: Anton Lundin <glance@acc.umu.se>

--- build.sh
+++ build.sh
@@ -13,6 +13,9 @@ elif [ "$ARCH" = "x86_64" ] ; then
 	BUILDCHAIN=x86_64-linux-android
 fi
 
+LIBTOOL_VERSION=2.4.6
+LIBSNDFILE_VERSION=1.0.26
+
 if [ ! -e ndk-$ARCH ] ; then
 	$ANDROID_NDK_ROOT/build/tools/make_standalone_toolchain.py --arch=$ARCH --install-dir=ndk-$ARCH --api=24
 fi
@@ -31,16 +34,16 @@ if [ ! -e pulseaudio ] ; then
 	git submodule update
 fi
 
-if [ ! -e libtool-2.4.6.tar.gz ] ; then
-	wget http://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz
+if [ ! -e libtool-$LIBTOOL_VERSION.tar.gz ] ; then
+	wget http://ftpmirror.gnu.org/libtool/libtool-$LIBTOOL_VERSION.tar.gz
 fi
-if [ ! -e libtool-2.4.6 ] ; then
-	tar -zxf libtool-2.4.6.tar.gz
+if [ ! -e libtool-$LIBTOOL_VERSION ] ; then
+	tar -zxf libtool-$LIBTOOL_VERSION.tar.gz
 fi
 if [ ! -e ${PREFIX}/lib/libltdl.a ] ; then
 	mkdir -p libtool-build-$ARCH
 	pushd libtool-build-$ARCH
-	../libtool-2.4.6/configure --host=${BUILDCHAIN} --prefix=${PREFIX} HELP2MAN=/bin/true MAKEINFO=/bin/true
+	../libtool-$LIBTOOL_VERSION/configure --host=${BUILDCHAIN} --prefix=${PREFIX} HELP2MAN=/bin/true MAKEINFO=/bin/true
 	make
 	make install
 	popd
@@ -49,16 +52,16 @@ fi
 # Now, use updated libtool
 export LIBTOOLIZE=${PREFIX}/bin/libtoolize
 
-if [ ! -e libsndfile-1.0.26.tar.gz ] ; then
-	wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.26.tar.gz
+if [ ! -e libsndfile-$LIBSNDFILE_VERSION.tar.gz ] ; then
+	wget http://www.mega-nerd.com/libsndfile/files/libsndfile-$LIBSNDFILE_VERSION.tar.gz
 fi
-if [ ! -e libsndfile-1.0.26 ] ; then
-	tar -zxf libsndfile-1.0.26.tar.gz
+if [ ! -e libsndfile-$LIBSNDFILE_VERSION ] ; then
+	tar -zxf libsndfile-$LIBSNDFILE_VERSION.tar.gz
 fi
 if [ ! -e $PKG_CONFIG_PATH/sndfile.pc ] ; then
 	mkdir -p libsndfile-build-$ARCH
 	pushd libsndfile-build-$ARCH
-	../libsndfile-1.0.26/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --disable-external-libs --disable-alsa --disable-sqlite
+	../libsndfile-$LIBSNDFILE_VERSION/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --disable-external-libs --disable-alsa --disable-sqlite
 	make ||:
 	make install ||:
 	cp sndfile.pc ${PREFIX}/lib/pkgconfig/
